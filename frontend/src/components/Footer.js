2e964db29bf5818e7438cefa6aa993bbea89e76a
Gladden Rumao <gladdenbackup@gmail.com>
2025-03-05 15:25:33 +0530

    Added footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer style={{ textAlign: "center", padding: "20px", fontSize: "18px" }}>
+        Made with <span style={{ color: "red" }}>❤️</span> by GG
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import Footer from "@/components/Footer"; // Import Footer
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="app-container">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
