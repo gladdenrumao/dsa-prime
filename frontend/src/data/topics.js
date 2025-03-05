10edc6d8f7d369ae39da0def226f2d4a0f9e9d29
Gladden Rumao <gladdenbackup@gmail.com>
2025-03-05 13:22:13 +0530

    Added all notes

---
+++ frontend/src/data/topics.js
@@ -0,0 +1,11 @@
+export const topics = [
+    { id: "binary-search", name: "Binary Search" },
+    { id: "bubble-sort", name: "Bubble Sort" },
+    { id: "selection-sort", name: "Selection Sort" },
+    { id: "insertion-sort", name: "Insertion Sort" },
+    { id: "recursion", name: "Recursion" }, 
+    { id: "time-space-complexity", name: "Time and Space Complexity" },
+    { id: "prefix-sum", name: "Prefix Sum" },
+    { id: "two-pointers", name: "Two Pointers" },
+  ];
+  
